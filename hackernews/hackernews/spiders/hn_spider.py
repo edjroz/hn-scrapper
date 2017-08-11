70028c2ecddce2045528dca33bff34c340bba05f
edjroz <eduardo.j.diaz.rodriguez@outlook.com>
2017-08-11 18:58:55 -0400

    spider creation

---
+++ hackernews/hackernews/spiders/hn_spider.py
@@ -0,0 +1,21 @@
+from scrapy import Spider
+from scrapy.selector import Selector
+
+from hackernews.items import HackerNewItem
+
+class HackerNewsSpider(Spider):
+    name = "hackernews"
+    allowed_domains = ["news.ycombinator.com"]
+    start_urls = [
+        "http://news.ycombinator.com/",        
+    ]
+    def parse(self,response):
+        sel = Selector(response)
+        news = sel.xpath("//tr[@class='athing']/td[3]")
+        
+        for new in news:
+            item = HackerNewItem()
+            item['title'] = new.xpath("a[@href]/text()").extract()[0]
+            item['url'] = new.xpath("a/@href").extract()[0]
+            print(item)
+            yield
